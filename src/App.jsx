05f5eed26fc33f3767b1cbfe95e61835cd43c9cf
Andeglieri <comoassimnaointendi@hotmail.com>
2024-09-21 00:48:34 -0300

    project init

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+    return (
+        <div className="min-h-screen max-hfull bg-zinc-950">
+            <div className="font-bold">App, teste</div>
+        </div>
+    )
+}
