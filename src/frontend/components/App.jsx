86d9fd3c30cd07222b1e48631d25281dec80f6c9
Andeglieri <comoassimnaointendi@hotmail.com>
2024-09-23 22:04:26 -0300

    Folder organization and architecture

--- src/frontend/components/App.jsx
+++ src/frontend/components/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 export default function App() {
+
+    localStorage.setItem("aqui", [])
     return (
         <div className="min-h-screen max-hfull bg-zinc-950">
             <div className="font-bold">App, teste</div>
